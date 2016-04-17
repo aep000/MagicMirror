ce8aca89b73018cb8b451ebcae7d4ccbcfa375e8
Alex Parson <alexparson1@gmail.com>
2016-04-17 01:30:45 -0400

    Convert to mysql

--- Speech Part/backendGet.php
+++ Speech Part/backendGet.php
@@ -11,7 +11,7 @@ if (!$conn) {
 }
 $query = "SELECT * FROM  `speech` WHERE  `ID` = ( SELECT MAX(  `ID` ) FROM  `speech` )";
 var_dump($query);
-$result = mysqli_query($conn, $sql);
+$result = mysqli_query($conn, $query);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {

ba0860d7b7c5d658199bf89fa0fb5a7779b19a81
Alex Parson <alexparson1@gmail.com>
2016-04-16 22:44:49 -0400

    WoRKS HERE

--- testing/js/index.js
+++ testing/js/index.js
@@ -5,7 +5,7 @@ recognition.interimResults = true;
 recognition.onresult = function(event) {
   if(event.results[0].isFinal==1){
   console.log(event.results[0][0].transcript)
-  $.post("http://172.31.168.171/MagicMirror/SpeechPart/backendAdd.php",{
+  $.post("http://172.31.168.171/MagicMirror/Speech Part/backendAdd.php",{
         text: event.results[0][0].transcript
     }
     ,function(data, status){

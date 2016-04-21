c8dfd6afa3c67073909db3e8f0a806897eecb817
Alex Parson <alexparson1@gmail.com>
2016-04-21 19:31:07 -0400

    testing audio

--- testing/js/index.js
+++ testing/js/index.js
@@ -5,13 +5,14 @@ recognition.interimResults = true;
 recognition.onresult = function(event) {
   if(event.results[0].isFinal==1){
   console.log(event.results[0][0].transcript)
-  $.post("http://172.31.168.171/MagicMirror/Speech Part/backendAdd.php",{
+  /*$.post("http://172.31.168.171/MagicMirror/Speech Part/backendAdd.php",{
         text: event.results[0][0].transcript
     }
     ,function(data, status){
         console.log("Data: " + data + "\nStatus: " + status);
     });
-  }
+  }*/
+  console.log(event.results[0][0].transcript.toLowerCase().indexOf("mirror mirror") == 0);
 }
 recognition.onend = function(){
   recognition.start();

a78219972e5a2b424ad10a068f21035f566f0ba7
Jake Sylvestre <jakesyl@gmail.com>
2016-04-23 15:49:01 -0400

    enable ajax

--- speechRecognition/js/index.js
+++ speechRecognition/js/index.js
@@ -5,6 +5,20 @@ recognition.interimResults = true;
 recognition.onresult = function(event) {
   if(event.results[0].isFinal==1 && event.results[0][0].transcript.toLowerCase().indexOf("mirror mirror") == 0){
   console.log(event.results[0][0].transcript)
+		$.ajax({
+			type: "GET",
+			url: "./create",
+			data: {
+				speech : event.results[0][0].transcript
+			},
+			success: function(data, status, xhr) {
+				console.log(data) //TODO check response type, delegate to functions
+			},
+			error: function(jqXHR, status, error) {
+				console.log(error);
+			}
+		});
+
 
 }
 }
